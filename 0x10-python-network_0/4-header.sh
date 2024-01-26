f1a61e664c316c14031600002efcc3968f48dd30
lavyatarah <mattlavyy@gmail.com>
2024-01-25 18:45:39 -0800

    upadate

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-
-if [ -z "$1" ] || [ -z "$2" ]; then
- echo "Usage: $0 <url> <filename>"
- exit 1
-fi
-
-url=$1
-filename=$2
-
-if [ ! -f "$filename" ]; then
- echo "File not found: $filename"
- exit 1
-fi
-
-response=$(curl -s -X POST -H "Content-Type: application/json" --data @"$filename" "$url")
-
-echo $response
+#takes in a URL as an argument, sends a GET request to the URL,and displays the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
