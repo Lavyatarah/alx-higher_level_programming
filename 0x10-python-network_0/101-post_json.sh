a5c7d3f5f12b63d992ab61a2dead8f9077ab0bc7
lavyatarah <mattlavyy@gmail.com>
2024-01-25 18:51:05 -0800

    json

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-
-if [ -z "$1" ]; then
- echo "Usage: $0 <url>"
- exit 1
-fi
-
-url=$1
-
-response=$(curl -s -H "X-School-User-Id: 98" "$url")
-
-echo $response
+# Sends a JSON POST request to a given URL with a given JSON file.
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
